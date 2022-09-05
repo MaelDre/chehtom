a9c44983963f29d6c9c2e7764aeb80c91a808a49
Maël <mael@bougetb.fr>
2022-09-05 15:11:55 +0200

    Improve the management of letter in double

--- src/components/ChehtomGame.vue
+++ src/components/ChehtomGame.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>Welcome in Chehtom</h1>
+  <h1>CHEHTOM</h1>
   <displayGame ref="grille" />
   <br />
   <input id="wordInput" type="text" name="inputWord" v-model="wordToTry" v-on:keyup.enter="tryWord(wordToTry)">
@@ -37,7 +37,9 @@ export default {
           {"w": "crochet", "cw":"groscheh"},
           {"w": "checkup", "cw":"chehcup"},
           {"w": "attacher", "cw":"attacheh"},
-          {"w": "chebmami", "cw":"chehbmami"}
+          {"w": "chebmami", "cw":"chehhbmami"},
+          {"w": "empecher", "cw":"empechehh"},
+          {"w": "toucher", "cw":"touchehhh"},
         ],
         letternb: 6,
         chehWordArray: ['p','i','r','a','t','e'],
@@ -52,110 +54,139 @@ export default {
   },
   methods: {
     async tryWord(w){
-        console.log("starting tryWord method... with the word", w);
-        // put the word in lower case
-        w = w.toLowerCase();
+      // this method  is the heart of the game
+      // it is called each time the player submits a word
+      
+      // put the word in lower case
+      w = w.toLowerCase();
+
+      // here we should manage the accents too
+      w = w.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+
+      // convert the string to an array
+      //let wordToTryArray = Array.from(w);
+
+      // we check if the letters matches
+      //for (let i=0; i < this.word.length; i++){
+      //    this.checkLetter(wordToTryArray[i],i,this.word);
+      //}
+      this.checkWord(w);
+
+      // display the word entered on the new grid Obj
+      this.$refs.grille.changeAWordObj(this.count,w, this.colorArray, this.letternb);
+      
+      // we wait the word to be displayed on the grid
+      await sleep(4000);
         
-        let rese = this.whereLetterIsInChehword('e',w);
-        console.log (rese);
+      // Ici il faudrait que je trouve un moyen d'attendre que le mot entré ait été affiché pour jouer la suite
+      // Il faudrait controler l'état de l'affichage avec un genre de jeton, pour empecher toute action tant que l'affichage n'est pas fini.
+      if (w === this.word) {
+          console.log("victory!");
+          this.win = 1;
+      }
+      else if ((w != this.word) && (this.count === 5)){
 
-        let wordToTryArray = Array.from(w);
-        console.log(wordToTryArray);
-        
-        // we check if the letters matches
-        //this.checkLetter(wordToTryArray[0],0,this.word);
-        //this.checkLetter(wordToTryArray[1],1,this.word);
-        //this.checkLetter(wordToTryArray[2],2,this.word);
-        //this.checkLetter(wordToTryArray[3],3,this.word);
-        //this.checkLetter(wordToTryArray[4],4,this.word);
-        //this.checkLetter(wordToTryArray[5],5,this.word);
+        // If not victory after 6 try, it is a defeat !
+        this.win = -1;
+        this.result_message ="Perdu Looser";
+      }
+      else if ((w != this.word) && (this.count < 5)){
 
-        // New checkLetter with a loop
-        for (let i=0; i < this.word.length; i++){
-          this.checkLetter(wordToTryArray[i],i,this.word);
-        }
+        console.log("not finished yet ")
+        this.win = 0;
+        this.count++;
+        //display the right letters on the grid
+        this.$refs.grille.displayFirstLetter(this.count,this.word);
 
-        // Obsolete : useless new
-        // display the word entered on the grid
-        //this.$refs.grille.changeAWord(this.count,w);
-
-        console.log("before changeAWordObj");
-        // display the wordentered on the new grid Obj
-        this.$refs.grille.changeAWordObj(this.count,w, this.colorArray, this.letternb);
-        await sleep(4000);
-        console.log("after changeAWordOb");
-        
-
-        // Ici il faudrait que je trouve un moyen d'attendre que le mot entré ait été affiché pour jouer la suite
-        // Il faudrait controler l'état de l'affichage avec un genre de jeton, pour empecher toute action tant que l'affichage n'est pas fini.
-        if (w === this.word) {
-            console.log("victory!");
-            this.win = 1;
-        }
-        else if ((w != this.word) && (this.count === 5)){
-          // If not victory after 6 try, it is a defeat !
-          this.win = -1;
-          this.result_message ="Perdu Looser";
-        }
-        else if ((w != this.word) && (this.count < 5)){
-            console.log("not finished yet ")
-            this.win = 0;
-            this.count++;
-            //display the right letters on the grid
-            this.$refs.grille.displayFirstLetter(this.count,this.word);
-
-            // put the focus on the text input field
-            wordInput.focus();
+        // put the focus on the text input field
+        wordInput.focus();
         }
     },
     whereLetterIsInChehword(l,cw){
-        // renvoie l'index de la position de la lettre dans le sheh word
-        // si absent : -1
-        return cw.search(l);
+      // renvoie l'index de la position de la lettre dans le sheh word
+      // si absent : -1
+      return cw.search(l);
     },
     checkLetter(l,i,cw){
         //this method checks if the letter is in the Word AND at the right place 
-        if (this.whereLetterIsInChehword(l,cw) === i){
+
+        // we check a letter and get its position in the cw
+        let position = this.whereLetterIsInChehword(l,cw)
+        if (position === i){
             console.log (l, 'est à sa place well done!');
             this.resArray[i] = l;
             this.colorArray[i] = "c2";
+            // ici il faudrait retirer la lettre de rang l du mot cw
+            return 2;
         }
-        else if (this.whereLetterIsInChehword(l,cw) >= 0){
+        else if (position >= 0){
             console.log(l, "is not at the good place but in the word");
             this.colorArray[i] = "c1";
+            return 1;
         }
         else{
             console.log(l, 'is not in the word');
             this.colorArray[i] = "c0";
+            return 0;
         }
         console.log('colorArray:', this.colorArray);
     },
+    checkWord(w){
+      // This methods compares the word submited with the word to find
+      // convert the string to an array
+
+      let tempWord = this.word;
+      let tempWordArray = Array.from(tempWord);
+      let wordToTryArray = Array.from(w);
+      let resCheckLetter = 0;
+
+      // we check if the letters matches
+      for (let i=0; i < this.word.length; i++){
+          resCheckLetter = this.checkLetter(wordToTryArray[i],i,tempWord);
+          console.log(resCheckLetter);
+
+          // if the letter has been found we update the word where to search in order to avoid doublon
+          // we replace the letter found by "."
+          if (resCheckLetter > 0){
+            tempWordArray[i] = "."
+            console.log(tempWordArray);
+            tempWord = tempWordArray.join("");
+          }
+        
+      }
+    },
     resetGame(){
-        // select a new chehword
-        // get random index value
-        let randomIndex = Math.floor(Math.random() * this.chehDict.length);
 
-        // get random item
-        this.word = this.chehDict[randomIndex].w;
+      // select a new chehword
+      // get random index value
+      let randomIndex = Math.floor(Math.random() * this.chehDict.length);
 
-        // get the associated cheh version of the word
-        this.chehWord = this.chehDict[randomIndex].cw;
+      // get random item
+      this.word = this.chehDict[randomIndex].w;
 
-        // count the number of letters
-        this.letternb = this.word.length;
+      // get the associated cheh version of the word
+      this.chehWord = this.chehDict[randomIndex].cw;
 
-        // reset counter
-        this.count = 0;
+      // count the number of letters
+      this.letternb = this.word.length;
 
-        // clean the grid
-        this.$refs.grille.cleanGrid2(this.letternb);
+      // reset counter
+      this.count = 0;
 
-        // displayFirstLetter
-        this.$refs.grille.displayFirstLetter(0,this.word);
+      // clean the grid
+      this.$refs.grille.cleanGrid2(this.letternb);
+
+      // displayFirstLetter
+      this.$refs.grille.displayFirstLetter(0,this.word);
     }
   },
   mounted() {
+    // this function is executed directly when the app is mounted
+
+    // we display the first letter of the word
     this.$refs.grille.displayFirstLetter(0,this.word);
+
+    // we put the focus on the word text field
     wordInput.focus();
   }
 }
